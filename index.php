22c964fd43522fe8d1d137aaeb27f743fc4c05bd
Hekar A. Mohammed <hekar1987@gmail.com>
2024-04-17 02:19:43 +0300

    Add Bootstrap card component and CDN links
    Added a Bootstrap card component to display content and included Bootstrap CDN links for styling.

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include "connect.php";
 
 <head>
     <title>SQL Query Executor</title>
+
 </head>
 
 <body>
@@ -48,8 +49,18 @@ include "connect.php";
     $conn = null;
     ?>
 
-</body>
+    <div class="card" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
 
+</body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js"></script>
 
 </html>
