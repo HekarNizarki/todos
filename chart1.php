e302c24a1727fdd810fffbaf4723c646ad33f9f7
Hekar A. Mohammed <hekar1987@gmail.com>
2024-04-29 22:55:54 +0300

    Update chart1.php background color settings
    Remove unnecessary background color settings in chart1.php file.

--- chart1.php
+++ chart1.php
@@ -43,10 +43,7 @@ foreach ($result as $row) {
                 tension: 0.5,
                 backgroundColor: [
                     'rgba(255, 99, 132, 0.3)',
-                    'rgba(255, 159, 64, 0.3)',
-                    'rgba(255, 205, 86, 0.3)',
-                    'rgba(75, 192, 192, 0.3)',
-                    'rgba(54, 162, 235, 0.3)',
+
 
                 ],
             }, ]
