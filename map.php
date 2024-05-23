48dc965159a787c3d56a4fde6d370d79b5de5389
Hekar A. Mohammed <hekar1987@gmail.com>
2024-05-24 00:15:26 +0300

    Update minZoom level on map tile layer in map.php file

--- map.php
+++ map.php
@@ -41,7 +41,7 @@
     var map = L.map('map').setView([39.8, -70], 4);
     L.tileLayer('https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=T8HqAMgrrkJfTxTB6c25', {
         // attribution: '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
-        minZoom: 4,
+        minZoom: 3,
         maxZoom: 19
     }, ).addTo(map);
 
